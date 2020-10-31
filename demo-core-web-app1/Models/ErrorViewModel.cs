4489a189b8346d6488bb3757d5c44ac3c5fb2619
neerajk06 <neerajk16@outlook.com>
2020-10-31 01:49:00 -0400

    sample .net core app

---
+++ demo-core-web-app1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demo_core_web_app1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
